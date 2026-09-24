8732a9da39de9b20c4ae5c949e60e0f7a0709182
Minty <aeternamsonne@outlook.com>
2026-09-24 18:17:18 +0700

    Added basic Node.h

--- Node.h
+++ Node.h
@@ -1,1 +1,28 @@
 #pragma once
+
+#include <cstddef>
+#include <limits>
+#include <optional>
+
+namespace astar {
+    using NodeId = std::size_t;
+
+    struct Node {
+        NodeId id;
+
+        double gScore;
+        double hScore;
+
+        std::optional<NodeId> parent;
+
+        explicit Node(NodeId id)
+            : id(id),
+            gScore(std::numeric_limits<double>::infinity()),
+            hScore(0.0),
+            parent(std::nullopt)
+        {}
+
+        [[nodiscard]]
+        double fScore() const noexcept {return gScore + hScore;}
+    };
+}
