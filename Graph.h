78ec0e855b89eb50e64abde86b7734d443608f1d
Minty <aeternamsonne@outlook.com>
2026-09-24 19:56:03 +0700

    Graph.h draft

--- Graph.h
+++ Graph.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "Node.h"
+
+#include <vector>
+
+namespace astar {
+    class Graph {
+    public:
+        virtual ~Graph() = default;
+
+        [[nodiscard]]
+        virtual std::vector<NodeId> getNeighbors(NodeId node) const = 0;
+
+        [[nodiscard]]
+        virtual double getCost(NodeId from, NodeId to) const = 0;
+    };
+}
--- Node.h
+++ Node.h
@@ -15,7 +15,7 @@ namespace astar {
 
         std::optional<NodeId> parent;
 
-        explicit Node(NodeId id)
+        Node(NodeId id)
             : id(id),
             gScore(std::numeric_limits<double>::infinity()),
             hScore(0.0),
