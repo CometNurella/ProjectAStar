6f10f444b6d994b2331228baf1f4605ea14b7822
Minty <aeternamsonne@outlook.com>
2026-09-24 20:09:20 +0700

    Added final Grid.h

--- Grid.h
+++ Grid.h
@@ -1,1 +1,48 @@
 #pragma once
+
+#include "Graph.h"
+#include "GridNode.h"
+
+#include <cstddef>
+#include <vector>
+
+namespace astar
+{
+    class Grid : public Graph
+    {
+    public:
+        Grid(std::size_t width, std::size_t height);
+
+        [[nodiscard]]
+        std::size_t getNodeCount() const noexcept override;
+
+        [[nodiscard]]
+        std::vector<Edge> getEdges(NodeId node) const override;
+
+        [[nodiscard]]
+        bool isInside(int x, int y) const noexcept;
+
+        [[nodiscard]]
+        bool isWalkable(int x, int y) const noexcept;
+
+        void setWalkable(int x, int y, bool walkable);
+
+        [[nodiscard]]
+        NodeId toNodeId(int x, int y) const;
+
+        [[nodiscard]]
+        GridNode toGridNode(NodeId node) const;
+
+        [[nodiscard]]
+        std::size_t getWidth() const noexcept;
+
+        [[nodiscard]]
+        std::size_t getHeight() const noexcept;
+
+    private:
+        std::size_t m_width;
+        std::size_t m_height;
+
+        std::vector<bool> m_walkable;
+    };
+}
