ec7a096e693931f2ef8769685e39330c307e1180
Minty <aeternamsonne@outlook.com>
2026-09-25 10:39:34 +0700

    Minor changes

--- Grid.cpp
+++ Grid.cpp
@@ -4,18 +4,13 @@
 
 namespace astar {
     Grid::Grid(std::size_t width, std::size_t height)
-        : m_width{ width },
-        m_height{ height },
-        m_walkable(width* height, true)
+        : m_width{ width }, m_height{ height }, m_walkable(width* height, true)
     {}
 
-    std::size_t Grid::getNodeCount() const noexcept {
-        return m_width * m_height;
-    }
+    std::size_t Grid::getNodeCount() const noexcept { return m_width * m_height; }
 
     bool Grid::isInside(int x, int y) const noexcept {
-        return x >= 0 &&
-            y >= 0 &&
+        return x >= 0 && y >= 0 &&
             static_cast<std::size_t>(x) < m_width &&
             static_cast<std::size_t>(y) < m_height;
     }
@@ -25,17 +20,14 @@ namespace astar {
             return false;
         }
         const NodeId id {
-            static_cast<NodeId>(y) * m_width +
-            static_cast<NodeId>(x)
+            static_cast<NodeId>(y) * m_width + static_cast<NodeId>(x)
         };
         return m_walkable[id];
     }
 
     void Grid::setWalkable(int x, int y, bool walkable) {
         if (!isInside(x, y)) {
-            throw std::out_of_range{
-                "Grid coordinate is outside the grid"
-            };
+            throw std::out_of_range{ "Grid coordinate is outside the grid" };
         }
         const NodeId id{ toNodeId(x, y) };
         m_walkable[id] = walkable;
@@ -43,24 +35,16 @@ namespace astar {
 
     NodeId Grid::toNodeId(int x, int y) const {
         if (!isInside(x, y)) {
-            throw std::out_of_range{
-                "Grid coordinate is outside the grid"
-            };
+            throw std::out_of_range{ "Grid coordinate is outside the grid" };
         }
-        return static_cast<NodeId>(y) * m_width
-            + static_cast<NodeId>(x);
+        return static_cast<NodeId>(y) * m_width + static_cast<NodeId>(x);
     }
 
     GridNode Grid::toGridNode(NodeId node) const {
         if (node >= getNodeCount()) {
-            throw std::out_of_range{
-                "Invalid node ID"
-            };
+            throw std::out_of_range{ "Invalid node ID" };
         }
-        return GridNode{
-            static_cast<int>(node % m_width),
-            static_cast<int>(node / m_width)
-        };
+        return GridNode{ static_cast<int>(node % m_width), static_cast<int>(node / m_width) };
     }
 
     std::vector<Edge> Grid::getEdges(NodeId node) const {
