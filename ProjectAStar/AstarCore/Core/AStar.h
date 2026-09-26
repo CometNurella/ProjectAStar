cd773d17677f1c2d737490cf41ea0155d373c7d0
Minty <aeternamsonne@outlook.com>
2026-09-26 20:09:25 +0700

    Organize files into folders

---
+++ ProjectAStar/AstarCore/Core/AStar.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Graph.h"
+#include "Node.h"
+#include "Heuristics.h"
+
+#include <cstddef>
+#include <limits>
+#include <vector>
+
+namespace astar {
+    struct SearchResult {
+        bool found{ false };
+
+        std::vector<NodeId> path{};
+
+        double cost{ std::numeric_limits<double>::infinity() };
+
+        std::size_t expandedNodes{ 0 };
+    };
+
+    class Astar {
+    private:
+        [[nodiscard]]
+        static std::vector<NodeId> reconstructPath(const std::vector<Node>& nodes, NodeId goal);
+
+    public:
+        [[nodiscard]]
+        SearchResult findPath(const Graph& graph, NodeId start, NodeId goal, const Heuristic& heuristic) const;
+    };
+}
